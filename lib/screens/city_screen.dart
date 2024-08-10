1161b47922756f381df57e3d89b2fe25e2d6af5e
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2024-08-10 17:53:36 +0300

    Costumize city screen

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -12,51 +12,43 @@ class _CityScreenState extends State<CityScreen> {
   String? cityName;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("images/city_background.jpg"),
-          fit: BoxFit.cover,
-        ),
-      ),
-      constraints: BoxConstraints.expand(),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Align(
-                alignment: Alignment.topLeft,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Icon(
-                    Icons.arrow_back_ios,
-                    size: 50.0,
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(20.0),
-                child: TextField(
-                  decoration: kTextFieldInputDecoration,
-                  onChanged: (value) {
-                    cityName = value;
-                  },
-                ),
-              ),
-              TextButton(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topLeft,
+              child: TextButton(
                 onPressed: () {
-                  Navigator.pop(context, cityName);
+                  Navigator.pop(context);
                 },
-                child: const Text(
-                  "Get Weather",
-                  style: kButtonTextStyle,
+                child: const Icon(
+                  Icons.arrow_back_ios,
+                  size: 50.0,
+                  color: Colors.black,
                 ),
               ),
-            ],
-          ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: kTextFieldInputDecoration,
+                onChanged: (value) {
+                  cityName = value;
+                },
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, cityName);
+              },
+              child: const Text(
+                "Get Weather",
+                style: kButtonTextStyle,
+              ),
+            ),
+          ],
         ),
       ),
     );
